7009c22a929874cdf433f2c19962d146523fee63
Sachin Yadav <sachin.yadav@csinfocomm.com>
2022-02-02 00:46:36 +0530

    Add Comments

--- src/components/Charts/Doughnut.js
+++ src/components/Charts/Doughnut.js
@@ -1,20 +1,8 @@
-// STEP 1 - Include Dependencies
-// Include react
 import React from "react";
-
-// Include the react-fusioncharts component
 import ReactFC from "react-fusioncharts";
-
-// Include the fusioncharts library
 import FusionCharts from "fusioncharts";
-
-// Include the chart type
 import Chart from "fusioncharts/fusioncharts.charts";
-
-// Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Chart);
-
-// STEP 3 - Creating the JSON object to store the chart configurations
 
 const ChartComponent = ({ data }) => {
   const chartConfigs = {
